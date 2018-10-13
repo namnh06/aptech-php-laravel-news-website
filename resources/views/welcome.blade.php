2d4af4368f3f49d2139d3c918210aa1ffa5faf94
[name] <namnh.website@gmail.com>
2018-10-13 11:24:25 +0700

    updated view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Relationship
+                    Laravel Relationship Checked
                 </div>
 
                 <div class="links">

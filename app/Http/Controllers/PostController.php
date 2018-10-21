98dcc7da8076d0bb9f230c84eec61e5da1eac997
[name] <namnh.website@gmail.com>
2018-10-21 19:15:05 +0700

    updated controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         //
-        return 'posts';
+        return Post::all();
     }
 
     /**

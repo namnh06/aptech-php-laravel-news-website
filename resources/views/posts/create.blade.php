83b75af147fb34038f357b9cac900477c9d4f10d
namnh-nick <nam.nguyenhai@yuriqa.com>
2019-07-05 15:12:27 +0700

    Added CKEditor & CKFinder

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,21 +6,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
-  <script src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
+  <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
+  <script src="{{asset('ckfinder/ckfinder.js')}}"></script>
 </head>
 
 <body>
   <textarea name="content" id="editor">This is some sample content.</textarea>
-  <script>
-    ClassicEditor
-                  .create( document.querySelector( '#editor' ) )
-                  .then( editor => {
-                          console.log( editor );
-                  } )
-                  .catch( error => {
-                          console.error( error );
-                  } );
-  </script>
 </body>
+<script>
+  CKEDITOR.replace('editor', {
+    filebrowserBrowseUrl: "{{asset('/ckfinder/ckfinder.html')}}",
+    filebrowserUploadUrl: "{{asset('/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files')}}"});
+</script>
 
 </html>
